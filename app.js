962894c2c03b6b3c4017b50a785f08c48dad399a
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-05-29 15:43:19 +0200

    add env file

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use(express.static(__dirname + "/node_modules"));
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: false}));
+//app.use(cookieParser());
 
 // Do we need this?
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+//app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'app')));
