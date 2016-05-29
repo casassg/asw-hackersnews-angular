82e90b66217fa249253f4d0c340a124fcee1b711
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-05-29 14:20:51 +0200

    style app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + "/node_modules"));
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 // Do we need this?
@@ -26,31 +26,31 @@ app.use(express.static(path.join(__dirname, 'app')));
 
 
 // catch 404 and forward to error handler
-app.use('*',function(req, res, next) {
-  res.sendFile(path.join(__dirname,'public/index.html'));
+app.use('*', function (req, res, next) {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // error handlers
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: {}
+    });
 });
 
 
