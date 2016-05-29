12cf79c6b64b414bfdc6903d2eafd0211d39515a
Gerard Casas <gerard.casas.saez@est.fib.upc.edu>
2016-05-29 20:49:07 +0200

    fix service getters

--- app/contribution/contribution.service.ts
+++ app/contribution/contribution.service.ts
@@ -20,17 +20,24 @@ export class ContributionService {
     constructor(private http:Http, private keeper:TokenKeeper) {
     }
 
-    private toContribution(json:any):Contribution{
+    private toPost(json:any):Contribution{
         let contribution = json.contribution;
         contribution.comments= json.comments;
         return contribution;
     }
+    private toComment(json:any):Contribution{
+        let contribution = json.contribution;
+        contribution.comments= json.replies;
+        return contribution;
+    }
+
+
 
     getAsks():Promise<Contribution[]> {
         return this.http.get(this.askUrl)
             .toPromise()
             .then(response => {
-                return response.json().data
+                return response.json()
             })
             .catch(this.handleError);
     }
@@ -38,14 +45,16 @@ export class ContributionService {
     getUrls():Promise<Contribution[]> {
         return this.http.get(this.urlUrl)
             .toPromise()
-            .then(response => response.json().data)
+            .then(response => {
+                return response.json()
+            })
             .catch(this.handleError);
     }
 
     getPost(id:number):Promise<Contribution> {
         return this.http.get(this.contributionsUrl + id)
             .toPromise()
-            .then(response => this.toContribution(response.json()))
+            .then(response => this.toPost(response.json()))
             .catch(this.handleError);
     }
 
@@ -54,23 +63,27 @@ export class ContributionService {
         let headers = new Headers({
             'Content-Type': 'application/json'
         });
+        let token = this.keeper.getToken();
+        if (!token) {
+            this.handleError("NOT LOGGED IN! YOU NEED TO LOGIN BEFORE THIS!")
+        }
+        headers.append('Authorization', token);
 
         if (contribution.contr_subtype == 'url') {
-            const url = contribution.url;
-            let parameters = {title, url};
+            let parameters = {'title':contribution.title, 'url': contribution.url};
             return this.http
                 .post(this.contributionsUrl, JSON.stringify(parameters), {headers: headers})
                 .toPromise()
-                .then(res => res.json().data)
+                .then(res => res.json())
                 .catch(this.handleError);
         }
         else {
             const content = contribution.content;
-            let parameters = {title, content};
+            let parameters = {'title':contribution.title, 'content':contribution.content};
             return this.http
                 .post(this.contributionsUrl, JSON.stringify(parameters), {headers: headers})
                 .toPromise()
-                .then(res => res.json().data)
+                .then(res => res.json())
                 .catch(this.handleError);
         }
 
@@ -80,22 +93,23 @@ export class ContributionService {
         let url = `${this.commentUrl}/${id}`;
         return this.http.get(url)
             .toPromise()
-            .then(response => response.json().data)
+            .then(response => this.toComment(response.json()))
             .catch(this.handleError);
     }
 
     postComment(text:string, parent:number):Promise<Contribution> {
-        const comment = text;
-        const parent_id = parent;
-        let parameters = {comment, parent_id};
+        let parameters = {'comment':text, 'parent_id':parent};
 
         let headers = new Headers();
         headers.append('Content-Type', 'application/json');
         let token = this.keeper.getToken();
+        if (!token) {
+            this.handleError("NOT LOGGED IN! YOU NEED TO LOGIN BEFORE THIS!")
+        }
         headers.append('Authorization', token);
 
         return this.http
-            .post(this.commentUrl, JSON.stringify({'comment':comment, 'parent_id':parent_id}), {headers: headers})
+            .post(this.commentUrl, JSON.stringify(parameters), {headers: headers})
             .toPromise()
             .then(res => res.json().data)
             .catch(this.handleError);
@@ -105,33 +119,44 @@ export class ContributionService {
         let url = `${this.replyUrl}/${id}`;
         return this.http.get(url)
             .toPromise()
-            .then(response => response.json().data)
+            .then(response => response.json())
             .catch(this.handleError);
     }
 
     postReply(contribution:Contribution):Promise<Contribution> {
         const reply = contribution.content;
         const parent_id = contribution.parent_id;
-        let parameters = {parent_id, reply};
+        let parameters = {'parent_id':contribution.parent_id, 'reply':contribution.content};
         let headers = new Headers({
             'Content-Type': 'application/json'
         });
 
+        let token = this.keeper.getToken();
+        if (!token) {
+            this.handleError("NOT LOGGED IN! YOU NEED TO LOGIN BEFORE THIS!")
+        }
+        headers.append('Authorization', token);
         return this.http
             .post(this.replyUrl, JSON.stringify(parameters), {headers: headers})
             .toPromise()
-            .then(res => res.json().data)
+            .then(res => res.json())
             .catch(this.handleError);
     }
 
     postVote(id:number){
       let headers = new Headers({
           'Content-Type': 'application/json'});
+        let token = this.keeper.getToken();
 
+        if (!token) {
+            this.handleError("NOT LOGGED IN! YOU NEED TO LOGIN BEFORE THIS!")
+        }
+        headers.append('Authorization', token);
+        let parameters = {'contribution_id':id};
       return this.http
-                   .post(this.newVote, JSON.stringify(id), {headers: headers})
+                   .post(this.newVote, JSON.stringify(parameters), {headers: headers})
                    .toPromise()
-                   .then(res => res.json().data)
+                   .then(res => res.json())
                    .catch(this.handleError);
     }
 
