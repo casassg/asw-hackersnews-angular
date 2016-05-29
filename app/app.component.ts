bb188aa2d3594795df51e582571e09e16ed20664
jmotger <joaquim.motger@est.fib.upc.edu>
2016-05-29 18:09:31 +0200

    added new test contr

--- app/app.component.ts
+++ app/app.component.ts
@@ -23,6 +23,7 @@ import { setCookie, getCookie }         from './user/cookies.helper';
       <a [routerLink]="['Dashboard']">Dashboard</a>
       <!--<a [routerLink]="['Newest']">Newest</a>-->
       <a [routerLink]="['ContributionDetail', {'id':'5'}]">Contribution 5 (test)</a>
+      <a [routerLink]="['ContributionDetail', {'id':'1'}]">Contribution 1 (test)</a>
       <a [routerLink]="['UserDetail',{id:1}]">Usuari 1 (test)</a>
       <a *ngIf="loggedIn" [routerLink]="['MeDetail']">Me</a>
       <a *ngIf="!loggedIn" [href]='login_url'>Login</a>
