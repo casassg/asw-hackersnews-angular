ee8745874ba028fa177552094391548ce4d64b8d
Pere Berge <pere.berge@est.fib.upc.edu>
2016-05-29 11:00:31 +0200

    newest improve

---
+++ app/newest.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { RouteParams } from '@angular/router-deprecated';
+
+import { Contribution }        from './contribution';
+import { ContributionService } from './contribution.service';
+@Component({
+  selector: 'my-newest',
+  templateUrl: 'app/newest.html',
+  styleUrls: ['app/newest.component.css']
+})
+export class NewestComponent implements OnInit {
+
+  contributions: Contribution[] = [];
+
+  constructor(
+    private _contributionService: ContributionService,
+    private _routeParams: RouteParams) {
+  }
+
+  ngOnInit() {
+    var _this = this;
+    return this._contributionService.getUrls()
+  }
+}
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
